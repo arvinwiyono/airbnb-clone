cdffa9dadd1c279dfc1f7fca3641ed1be6a2694f
Arvin <arvinwiyono@gmail.com>
2015-09-24 15:18:02 +1000

    Add views for authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
