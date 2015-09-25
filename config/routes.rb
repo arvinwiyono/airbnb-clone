e8a661d4ebbc4d0f284fe2a0aa74af4d7ab8934f
Arvin <arvinwiyono@gmail.com>
2015-09-25 14:09:06 +1000

    Update views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'pages/home'
+  root 'pages#home'
 
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
