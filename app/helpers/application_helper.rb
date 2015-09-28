679619965ee0a0c72409f399c8f3fd02fa45d5f5
Arvin <arvinwiyono@gmail.com>
2015-09-28 16:38:20 +1000

    Add gravatar in app helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+	def avatar_url(user)
+		gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+		"https://www.gravatar.com/avatar/#{gravatar_id}.jpg?d=identical&s=150"
+	end
+
 end
