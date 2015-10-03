58c02ca0b52054c9170084c53c42956ffa5edfc9
Arvin Wiyono <arvinwiyono@gmail.com>
2015-10-03 16:06:03 +1000

    Use facebook avatar if sign in from facebook

---
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -0,0 +1,12 @@
+class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+	def facebook
+		@user = User.from_omniauth(request.env['omniauth.auth'])
+		if @user.persisted?
+			sign_in_and_redirect @user, event: :authentication
+			set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
+		else
+			session['devise.facebook_data'] = request.env['omniauth.auth']
+			redirect_to new_user_registration_url
+		end
+	end
+end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,11 @@ module ApplicationHelper
 
 	def avatar_url(user)
 		gravatar_id = Digest::MD5::hexdigest(user.email).downcase
-		"https://www.gravatar.com/avatar/#{gravatar_id}.jpg?d=identical&s=150"
+		if user.image
+			user.image
+		else
+			"https://www.gravatar.com/avatar/#{gravatar_id}.jpg?d=identical&s=150"
+		end
 	end
 
 end
