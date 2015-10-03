3cf29e6dec9dc413ef14b32ceef93e9081974594
Arvin Wiyono <arvinwiyono@gmail.com>
2015-10-03 16:05:40 +1000

    Update user models

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,22 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :confirmable
+         :confirmable, :omniauthable
   validates :fullname, length: { maximum: 50 }, presence: true  
+
+  def self.from_omniauth(auth)
+    user = User.where(email: auth.info.email).first
+    if user
+    	return user
+    else
+    	where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.fullname = auth.info.name
+			user.provider = auth.provider
+			user.uid = auth.uid
+			user.email = auth.info.email
+			user.image = auth.info.image
+			user.password = Devise.friendly_token[0, 20]
+    	end
+    end
+  end
 end
