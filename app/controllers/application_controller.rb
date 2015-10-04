4453ffd414c641127ef411b81293b7a05740d61c
Arvin Wiyono <arvinwiyono@gmail.com>
2015-10-05 00:37:36 +1100

    Update profile without password

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
 
 	  def configure_permitted_parameters
 	  	devise_parameter_sanitizer.for(:sign_up) << :fullname
-	  	devise_parameter_sanitizer.for(:account_update) << :fullname
+	  	devise_parameter_sanitizer.for(:account_update) << :fullname << :phone_number << :description
 	  end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-  
+
   root 'pages#home'
-  devise_for :users, 
-			  path: '', 
-			  path_names: {sign_in: 'login', sign_out: 'logout', edit: 'profile'},
-			  controllers: {omniauth_callbacks: 'omniauth_callbacks'}
-  
+  devise_for :users,
+             path: '',
+             path_names: {sign_in: 'login', sign_out: 'logout', edit: 'profile'},
+             controllers: {
+                 omniauth_callbacks: 'omniauth_callbacks',
+                 registrations: 'registrations'
+             }
+
   resources :users, only: :show
 end
