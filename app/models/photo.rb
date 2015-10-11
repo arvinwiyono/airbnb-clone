ab47f4e6ca62a45ad544a93d88b2c7f1e8cd2316
Arvin Wiyono <arvinwiyono@gmail.com>
2015-10-11 18:29:56 +1100

    Update view for uploading room images and display them

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,5 +2,5 @@ class Photo < ActiveRecord::Base
   belongs_to :room
 
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
-  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
