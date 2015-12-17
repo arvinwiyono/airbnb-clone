208e151c4f0b439f751dc2dfb4ab780e74cf8345
Arvin Wiyono <arvinwiyono@gmail.com>
2015-12-17 18:52:34 +0700

    Match code with original

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,23 +1,22 @@
 class ReservationsController < ApplicationController
-
 	before_action :authenticate_user!
-
-	def create
-		@reservation = current_user.reservations.create(reservation_params)
-		redirect_to @reservation.room, notice: "Your reservation has been created"
-	end
 
 	def preload
 		room = Room.find(params[:room_id])
 		today = Date.today
-		reservations = room.reservations.where("start_date > ? OR end_date >= ?", today, today)
-		render json: reservations
+		reservations = room.reservations.where("start_date >= ? OR end_date >= ?", today, today)
+
+		render json: reservations	
+	end
+
+	def create
+		@reservation = current_user.reservations.create(reservation_params)
+
+		redirect_to @reservation.room, notice: "Your reservation has been created..."
 	end
 
 	private
-
-	def reservation_params
-		params.require(:reservation).permit(:start_date, :end_date, :price, :total, :room_id)
-	end
-
+		def reservation_params
+			params.require(:reservation).permit(:start_date, :end_date, :price, :total, :room_id)
+		end
 end
